d90ad77b6e26c48754e073799e7a94b343d7f7da
Bogdan1337 <diri.xddd@gmail.com>
2020-02-29 11:20:52 +0200

    no message

--- oop/src/CercMain.java
+++
@@ -1,9 +0,0 @@
-public class CercMain {
-    public static void main(String[] args) {
-
-        Cerc cerc = new Cerc(2);
-        System.out.println(cerc.aria());
-
-    }
-
-}
--- oop/src/StockMain.java
+++
@@ -1,6 +0,0 @@
-public class StockMain {
-    public static void main(String[] args) {
-       Stock stock = new Stock("tigari","kent",24,30);
-        System.out.println(stock.getChangedPercent());
-    }
-}
